6a482ed0a7ee583396149e56f2bba20f7c6820d9
Eric Peterson <elpeterson16@gmail.com>
2025-06-09 21:09:45 -0400

    fix(client): update endpoint

--- client/lib/apollo-client.ts
+++ client/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   // uri: 'http://localhost:4000',
-  uri: 'https://server.elpeterson.com',
+  uri: 'https://api.elpeterson.com',
   cache: new InMemoryCache(),
 });
 
