dd347889839a4465482f87a5b52195506e8659cd
Eric Peterson <elpeterson16@gmail.com>
2025-06-11 13:27:33 -0400

    build(client): update env files

--- client/lib/apollo-client.ts
+++ client/lib/apollo-client.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  // uri: 'http://localhost:4000',
-  uri: 'https://api.elpeterson.com',
+  uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
